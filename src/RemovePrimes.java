6ec6d6837d80c342c43e43b9aa013f51c7527ca9
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-06-04 02:53:39 +0530

    q6

---
+++ src/RemovePrimes.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class RemovePrimes {
+    public static boolean isprime(int val){
+        for(int i=2;i*i<=val;i++){
+            if(val%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void solution(ArrayList<Integer> al){
+        // write your code here
+        for(int i=al.size();i>=0;i--){
+            int val= al.get(i);
+            if(isprime(val)==true){
+                al.remove(i);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i = 0 ; i < n; i++){
+            al.add(scn.nextInt());
+        }
+        solution(al);
+        System.out.println(al);
+    }
+
+}
