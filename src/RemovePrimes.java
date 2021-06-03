32ac1677d56827ed72978ddb8203d0deac6e958d
Yashvardhan18 <vardhanyash20000@gmail.com>
2021-06-04 02:55:02 +0530

    q6

--- src/RemovePrimes.java
+++ src/RemovePrimes.java
@@ -13,7 +13,7 @@ public class RemovePrimes {
 
     public static void solution(ArrayList<Integer> al){
         // write your code here
-        for(int i=al.size();i>=0;i--){
+        for(int i=al.size()-1;i>=0;i--){
             int val= al.get(i);
             if(isprime(val)==true){
                 al.remove(i);
