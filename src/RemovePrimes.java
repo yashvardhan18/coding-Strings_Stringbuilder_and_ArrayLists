72feac70fcd45b719ee504e52744800eba7f1384
Yashvardhan18 <vardhanyash20000@gmail.com>
2021-06-19 23:32:00 +0530

    q6

--- src/RemovePrimes.java
+++ src/RemovePrimes.java
@@ -20,6 +20,7 @@ public class RemovePrimes {
             }
         }
     }
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
